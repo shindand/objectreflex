cc7169294384fefd09e60d98d5e0fe21112bbfb9
warvic <warvic@yandex.ru>
2023-06-16 12:38:54 +0300

    тесты работают

--- src/js/basic.js
+++ src/js/basic.js
@@ -6,33 +6,35 @@ export function sum(items) {
   return result;
 }
 
+const obj = { name: "мечник", health: 10, level: 2, attack: 80, defence: 40 };
 
-const obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40};
+export function orderByProps(obj, arrChange) {
+  let arr = [];
 
-export function orderByProps(obj, arrChange){
-  
-let arr= [];
-let obj3 = {};
-  
-  for (let key in obj) {
-  obj3.key = key;
-  obj3.value = obj[key];
-   arr.push(obj3);
-  obj3 = {};
-}
-const arrSort = arr.sort((x, y) => x.key.localeCompare(y.key));
-let arrSortFinish = [];
+  const objectArray = Object.entries(obj);
+  objectArray.forEach(([key, value]) => {
+    arr.push({ key: key, value: value });
+  });
 
-for (let i = 0; i < arrChange.length; i++) {
-   arrSortFinish = arrSort.reduce((acc, item, index) => {
-     if (item.key == arrChange[i]){   
-       arrSort.splice(i, 0, item);
-       arrSort.splice(index + 1 + i, 1);
-       return arrSort;
+  let arrSort = arr.sort((x, y) => (x.key > y.key ? 1 : -1));
+  let arrSort2 = arrSort;
+  let arrSortFinish = [];
+
+  for (let i = 0; i < arrChange.length; i++) {
+    arrSortFinish = arrSort.filter((item) => {
+      return item.key != arrChange[i];
+    });
+    arrSort = arrSortFinish;
+  }
+
+  for (let i = arrChange.length - 1; i >= 0; i--) {
+    for (let k = 0; k < arrSort2.length; k++) {
+      if (arrSort2[k].key == arrChange[i]) {
+        arrSortFinish.unshift(arrSort2[k]);
       }
-   },0);
-}
+    }
+  }
   return arrSortFinish;
 }
-  
-console.log(orderByProps(obj, ["name", "level"]));
+
+orderByProps(obj, ["name", "level"]);
